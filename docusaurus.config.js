9ad513b3429fcd4d1d378a330ecf2cb1d5a66082
realravenss <poggysalt@gmail.com>
2026-07-14 03:23:59 +0200

    attemping to fix site part 3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,8 +27,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'realravenss', // Your GitHub org/user name.
+  projectName: 'realravenss-docs', // Your repo name.
 
   onBrokenLinks: 'warn',
 
